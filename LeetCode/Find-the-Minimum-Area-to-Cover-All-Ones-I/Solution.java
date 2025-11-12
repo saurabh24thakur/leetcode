e7712f4fb0c2b5c9a82720bfa5b3b57d752a5a17
Saurabh Singh <saurabh5532u@gmail.com>
2025-11-12 16:16:08 +0530

    Add solution for Find the Minimum Area to Cover All Ones I

---
+++ LeetCode/Find-the-Minimum-Area-to-Cover-All-Ones-I/Solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public  int minimumArea(int[][] grid) {
+        int minRow = grid.length, maxRow = -1;
+        int minCol = grid[0].length, maxCol = -1;
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 1) {
+                    if (i < minRow) minRow = i;
+                    if (i > maxRow) maxRow = i;
+                    if (j < minCol) minCol = j;
+                    if (j > maxCol) maxCol = j;
+                }
+            }
+        }
+
+        int height = maxRow - minRow + 1;
+        int width = maxCol - minCol + 1;
+
+        return height * width;
+    }
+}
