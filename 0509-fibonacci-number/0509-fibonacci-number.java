da8876b446a0d96bfd1ab3257e382a6c62828f72
Saurabh Singh <saurabh5532u@gmail.com>
2026-01-09 21:34:19 +0530

    Time: 0 ms (100%), Space: 42.2 MB (33.9%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,10 +1,20 @@
 class Solution {
+    Map<Integer,Integer> map=new HashMap<>(); 
+
     public int fib(int n) {
+        if(map.containsKey(n)){
+            return map.get(n);
+        }
         if(n<=1){
             return n;
         }
 
-        return fib(n-1)+fib(n-2);
+
+        int res=fib(n-1)+fib(n-2);
+
+        map.put(n,res);
+
+        return res;
         
     }
 }
