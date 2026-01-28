736614b327319ac5463c1234a38534dab02ac9f6
Saurabh Singh <saurabh5532u@gmail.com>
2026-01-28 20:58:51 +0530

    Time: 1 ms (99.82%), Space: 55.8 MB (25.47%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,6 +1,25 @@
-class Solution {
-    public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        return nums[nums.length/2];
+public class Solution {
+    public int majorityElement(int[] num) {
+        int majority = num[0];
+        int votes = 1;
+
+        for (int i = 1; i < num.length; i++) {
+            if (votes == 0) {
+                majority = num[i];
+                votes = 1;
+            } else if (majority == num[i]) {
+                votes++;
+            } else {
+                votes--;
+            }
+        }
+
+        return majority;
     }
+
+    // public static void main(String[] args) {
+    //     Solution sol = new Solution();
+    //     int[] testInput = {2, 2, 2, 2, 2, 2, 1};
+    //     System.out.println(sol.majorityElement(testInput));
+    // }
 }
